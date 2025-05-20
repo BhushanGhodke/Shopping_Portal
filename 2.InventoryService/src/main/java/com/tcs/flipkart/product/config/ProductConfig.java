ff6f6f3b35b669d2db687c18c8816426500e12b3
JavaDeveloper <121335823+BhushanGhodke@users.noreply.github.com>
2025-05-20 09:28:20 +0530

    Update ProductConfig.java

--- 2.InventoryService/src/main/java/com/tcs/flipkart/product/config/ProductConfig.java
+++ 2.InventoryService/src/main/java/com/tcs/flipkart/product/config/ProductConfig.java
@@ -15,8 +15,8 @@ public class ProductConfig {
 	public Cloudinary getCloudinary() {
 		Map config= new HashMap();		
 		config.put("cloud_name","do1glzsfu");
-		config.put("api_key","948695311846617");
-		config.put("api_secret","Xi3b-S2vdGu-eNaHQ7fLJRLPH30");
+		config.put("api_key","");
+		config.put("api_secret","");
 		config.put("secure", "true");
 		return new Cloudinary(config);
 	}
